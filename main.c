b4945cb4ac6b8c6b9d3f0ed02c0be1e3f1eed9df
kate <khy2010@nyu.edu>
2023-02-13 19:10:03 -0500

    moved prompt creation to its own file

--- main.c
+++ main.c
@@ -1,26 +1,23 @@
 #include <ctype.h>
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
+
+#include "prompt.h"
+
 
 /* SOURCES USED:
 https://pubs.opengroup.org/onlinepubs/007904975/functions/getcwd.html
 https://www.w3schools.com/c/c_user_input.php
 //https://www.cyberciti.biz/tips/howto-linux-unix-bash-shell-setup-prompt.html
+https://stackoverflow.com/questions/22452314/how-to-get-the-current-working-directorycurrent-working-folder-name-instead-of
+https://www.qnx.com/developers/docs/6.4.0/neutrino/lib_ref/g/getcwd.html
 */
 
 //docker run -i --name cs202 --privileged --rm -t -v ~/Documents/cs202/labs:/cs202 -w /cs202 ytang/os bash
 
 int main(){
-    char command[1000]; 
-    char cwd[1024];
-    getcwd(cwd, 1024);
-
     while (1){
-        printf("[nyush ");
-        printf(cwd);
-        printf("]$ ");
-        fflush(stdout);
-        scanf("%s", command);
+        createPrompt();
     }
 
     return 0;
