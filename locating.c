3854fe521f989cb855c03337e680d19df5f0381a
kate <khy2010@nyu.edu>
2023-02-21 22:48:08 -0500

    filled in params

--- locating.c
+++ locating.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <libgen.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "locating.h"
 
@@ -10,29 +11,27 @@
  * @brief 
  * 
  */
-void ls(char *command){
-
+void ls(char **argv){
     pid_t pid;
+    int exitStatus; 
+    argv[0] = "/bin/ls";
+
+    
     pid = fork();
 
-    //fork fails
+    // fork failed (this shouldn't happen)
     if (pid < 0) {
         fprintf(stderr, "Error: Failed to fork\n"); 
         exit(0);
-    
+    } 
     // child (new process)
-    } else if (pid == 0) {
-        execv("/bin/ls", "", ...); 
-
-        printf("execv() failed\n"); //exec failed
-
-
+    else if (pid == 0) {
+        execv(argv[0], argv); 
+        fprintf(stderr, "Error: Invalid Program\n"); 
+    
+    // parent
     } else {
-        //wait for child process
-        waitpid(-1, ...); 
-  }
+        waitpid(-1, &exitStatus ,0);
+    }
 }
-
-
-
 
