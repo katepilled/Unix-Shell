7fce4d8caa6e398ea925b330a24373fe845726e1
kate <khy2010@nyu.edu>
2023-02-22 21:50:45 -0500

    create file for built in commands

---
+++ builtIn.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "builtIn.h"
+
+/**
+ * @brief 
+ * 
+ * @param argv user inputted command which specifies cd
+ */
+void changeDirectory(char **argv){
+    chdir
+
+}
---
+++ builtIn.h
@@ -0,0 +1,7 @@
+#ifndef _BUILTIN_H_
+#define _BUILTIN_H
+
+void changeDirectory(char **argv);
+
+
+#endif
