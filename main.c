ea68f978a3c24b4207146451c5bc40688d32bcc0
kate <khy2010@nyu.edu>
2023-02-22 21:51:06 -0500

    moved getting input to prompt.c

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ https://stackoverflow.com/questions/22452314/how-to-get-the-current-working-dire
 https://www.qnx.com/developers/docs/6.4.0/neutrino/lib_ref/g/getcwd.html
 https://www.tutorialspoint.com/c_standard_library/string_h.htm
 https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
-
 */
 
 //docker run -i --name cs202 --privileged --rm -t -v ~/Documents/cs202/labs:/cs202 -w /cs202 ytang/os bash
@@ -21,21 +20,11 @@ https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-
 //gcc main.c prompt.c locating.c
 
 int main(){
-    char command[1001];
     char **argv = NULL; 
 
     while (1){
 
-        createPrompt();
-        
-        //get user input/command
-        fgets(command, 1001, stdin);
-
-        //remove terminating newline char
-        command[strcspn(command, "\n")] = 0;
-
-        //format to char**
-        argv = formatCommand(command);    
+        argv = createPrompt(); 
 
         //ls function
         if (strcmp(argv[0], "ls\n") == 0 || strcmp(argv[0], "ls") == 0){
